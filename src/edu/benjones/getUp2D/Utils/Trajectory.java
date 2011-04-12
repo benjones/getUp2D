324a6a22442c88d04b517a53a66692afcfa4e190
Ben Jones <ben.james.jones@gmail.com>
2011-04-12 14:22:57 -0700

    Created Trajectory.  TODO: Add "Interpolator" object to do interpolation, make it generic and take vectors.  Non-priority

---
+++ src/edu/benjones/getUp2D/Utils/Trajectory.java
@@ -0,0 +1,74 @@
+package edu.benjones.getUp2D.Utils;
+
+import java.util.TreeSet;
+
+public class Trajectory {
+	public static class entry implements Comparable<Object> {
+		public float t;
+		public float val;
+
+		// stupid generics :(
+		@Override
+		public int compareTo(Object o) {
+			return (int) Math.signum((t - ((entry) o).t));
+		}
+
+		public entry(float t, float val) {
+			this.t = t;
+			this.val = val;
+		}
+
+		// sets val to null, used for searching
+		public entry(float t) {
+			this.t = t;
+			this.val = 0;
+		}
+	}
+
+	private TreeSet<entry> data;
+
+	public Trajectory(){
+		data = new TreeSet<entry>();
+	}
+	
+	public void clear(){
+		data.clear();
+	}
+	
+	public void addKnot(entry e) {
+		data.add(e);
+	}
+
+	public float getMinT() {
+		return data.first().t;
+	}
+
+	public float getMaxT() {
+		return data.last().t;
+	}
+
+	public float evaluateLinear(float t) {
+		entry before = data.floor(new entry(t));
+		entry after = data.ceiling(new entry(t));
+
+		if (before == null) {
+			if (after == null) {
+				System.out.println("warning: evaluated empty trajectory");
+				return 0;// this probably makes sense
+			} else {
+				return after.val;
+			}
+		} else {
+			if (after == null)
+				return before.val;
+			if (MathUtils.floatEquals(before.t, t))
+				return before.val;
+			if(MathUtils.floatEquals(after.t, t))
+				return after.val;
+			float a = before.val;
+			float b = after.val;
+			return (a + (b - a) * t / (after.t - before.t));
+		}
+	}
+
+}
---
+++ test/edu/benjones/getUp2D/test/TrajectoryTest.java
@@ -0,0 +1,35 @@
+package edu.benjones.getUp2D.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.benjones.getUp2D.Utils.MathUtils;
+import edu.benjones.getUp2D.Utils.Trajectory;
+
+
+public class TrajectoryTest {
+
+	private Trajectory traj;
+	@Before
+	public void setupTrajectory(){
+		traj = new Trajectory();
+		traj.addKnot(new Trajectory.entry(0,1));
+		traj.addKnot(new Trajectory.entry(1,2));
+		traj.addKnot(new Trajectory.entry(2,3));
+	}
+	
+	@Test
+	public void testTrajectory(){
+		assertEquals(MathUtils.floatEquals(traj.getMinT(), 0), true);
+		assertEquals(MathUtils.floatEquals(traj.getMaxT(), 2), true);
+		assertEquals(MathUtils.floatEquals(traj.evaluateLinear(-1), 1), true);
+		assertEquals(MathUtils.floatEquals(traj.evaluateLinear(4), 3), true);
+		assertEquals(MathUtils.floatEquals(traj.evaluateLinear(1), 2), true);
+		assertEquals(MathUtils.floatEquals(traj.evaluateLinear(2), 3), true);
+		assertEquals(MathUtils.floatEquals(traj.evaluateLinear(0), 1), true);
+		assertEquals(MathUtils.floatEquals(traj.evaluateLinear(.5f), 1.5f), true);
+		
+	}
+}
