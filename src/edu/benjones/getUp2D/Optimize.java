2c6c0c248b8a580d0a6b86aad2c851c9fcdf87f6
Ben Jones <ben.james.jones@gmail.com>
2011-04-29 16:55:28 -0700

    Trying to draw the optimization stuff... not working well.

--- src/edu/benjones/getUp2D/Optimize.java
+++ src/edu/benjones/getUp2D/Optimize.java
@@ -1,13 +1,17 @@
 package edu.benjones.getUp2D;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.jbox2d.testbed.ProcessingDebugDraw;
+
+import processing.core.PApplet;
 import edu.benjones.getUp2D.Utils.FileUtils;
 import edu.benjones.getUp2D.optimization.OptimizationThread;
 
 public class Optimize {
 
-	protected final static int numThreads = 16;
+	protected final static int numThreads = 1;
 
 	private static boolean stop;
 
@@ -31,18 +35,21 @@ public class Optimize {
 		OptimizationThread[] threads = new OptimizationThread[numThreads];
 		Thread[] threadContainers = new Thread[numThreads];
 		for (int i = 0; i < numThreads; ++i) {
-			threads[i] = new OptimizationThread(null);
+			threads[i] = new OptimizationThread(new ProcessingDebugDraw(
+					new PApplet()));
 		}
 
 		float[] initialParameters = FileUtils
-				.readParameters("./SPParameters/bestSoFar");
+				.readParameters("./SPParameters/dart.par");
+		float[] limits = FileUtils.readParameters("./SPParameters/limits.par");
+		assert (initialParameters.length == limits.length);
 		for (int i = 0; i < numThreads; ++i)
-			threads[i].setParameterMaxDelta(FileUtils
-					.readParameters("./SPParameters/limits.par"));
+			threads[i].setParameterMaxDelta(limits);
 
 		int iteration = 0;
 		int improvements = 0;
 		float bestSoFar = Float.POSITIVE_INFINITY;
+		ArrayList<Float> costOverTime = new ArrayList<Float>();
 		while (!stop) {
 			iteration++;
 			for (int i = 0; i < numThreads; ++i) {
@@ -73,22 +80,34 @@ public class Optimize {
 			if (minCost < bestSoFar) {
 				improvements++;
 				initialParameters = threads[minIndex].getUpdatedParameters();
+				bestSoFar = minCost;
 				System.out.println("bestSoFar improved " + improvements
 						+ " times");
+				costOverTime.add(minCost);
 				FileUtils.writeParameters("./SPParameters/bestSoFar",
 						initialParameters);
 			} else {
 				System.out.println("Nothing better than bestSoFar");
+				if (minCost == Float.POSITIVE_INFINITY) {
+					System.out.println("all failures: " + iteration);
+					FileUtils.writeParameters(
+							"./SPParameters/FAIL" + iteration,
+							threads[0].getUpdatedParameters());
+				}
 			}
 
 			System.out.println("minCost is: " + minCost);
 			if (iteration % 100 == 0) {
-				FileUtils.writeParameters("./SPParameters/0428iteration"
+				FileUtils.writeParameters("./SPParameters/0429iteration"
 						+ iteration + ".par", initialParameters);
 			}
+
 		}
 
 		System.out.println("stopping after iteration: " + iteration);
+		System.out.println("Costs: ");
+		for (float c : costOverTime)
+			System.out.println(c);
 
 	}
 }
